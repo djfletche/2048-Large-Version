d3e5c7abfc44a6128fe4b5d111efe2f98cfc3ab4
djfletche <46875948+djfletche@users.noreply.github.com>
2019-10-09 22:52:10 -0400

    Update DrawUtils.java

--- GameRemake/src/DrawUtils.java
+++ GameRemake/src/DrawUtils.java
@@ -6,18 +6,30 @@ import java.awt.geom.Rectangle2D;
 public class DrawUtils 
 {
 	
-	private DrawUtils() { }
-	
+	private DrawUtils() 
+	{
+		
+	}
+	// This is used to get the width and center it on the screen
+	// checks how wide the message is and center that on the tile
+	// This is used to center the text because it makes thigs easier on the tile class
 	public static int getMessageWidth(String message, Font font, Graphics2D g) 
 	{
 		g.setFont(font);
+		//choose the font then get the boundaries and sets the width accordingly
 		Rectangle2D bounds = g.getFontMetrics().getStringBounds(message, g);
+		
 		return (int)bounds.getWidth();
+		
 	}
+	
 	public static int getMessageHeight(String message, Font font, Graphics2D g) 
 	{
-		g.setFont(font);
-		if(message.length() == 0) return 0;
+		g.setFont(font);//padding is buit into the height of a font
+		
+		if(message.length() == 0) return 0;// if the length is 0 just return
+		// textLayout is used to get the size and bound of a message. or text
+		
 		TextLayout tl = new TextLayout(message, font, g.getFontRenderContext());
 		return (int) tl.getBounds().getHeight();
 	}
